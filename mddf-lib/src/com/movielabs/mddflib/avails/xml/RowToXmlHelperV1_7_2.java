414ed10b3b1e2eb59b6efab61712bc27224f42f8
Larry Levin <lawrence.j.levin@gmail.com>
2018-06-08 15:31:22 -0400

    Bug fix re attributes of FormatProfile

--- mddf-lib/src/com/movielabs/mddflib/avails/xml/RowToXmlHelperV1_7_2.java
+++ mddf-lib/src/com/movielabs/mddflib/avails/xml/RowToXmlHelperV1_7_2.java
@@ -23,26 +23,79 @@
 package com.movielabs.mddflib.avails.xml;
 
 import org.apache.poi.ss.usermodel.Row;
+import org.jdom2.Element;
+import org.jdom2.Namespace;
+
+import com.movielabs.mddflib.logging.LogMgmt;
 
 /**
- * Create XML document from a v1.7.2 Excel spreadsheet. The XML generated will be
- * based on v2.2 of the Avails XSD and reflects a "best effort" in that there is
- * no guarantee that it is valid. 
+ * Create XML document from a v1.7.2 Excel spreadsheet. The XML generated will
+ * be based on v2.2 of the Avails XSD and reflects a "best effort" in that there
+ * is no guarantee that it is valid.
  * 
  * @author L. Levin, Critical Architectures LLC
  *
  */
-public class RowToXmlHelperV1_7_2 extends RowToXmlHelperV1_7  {
+public class RowToXmlHelperV1_7_2 extends RowToXmlHelperV1_7 {
 
-	static final String MISSING = "--FUBAR (missing)"; 
+	static final String MISSING = "--FUBAR (missing)";
 
 	/**
 	 * @param fields
 	 */
-	RowToXmlHelperV1_7_2(AvailsSheet sheet, Row row) {
-		super(sheet, row); 
+	RowToXmlHelperV1_7_2(AvailsSheet sheet, Row row, LogMgmt logger) {
+		super(sheet, row, logger);
 	}
- 
-   
+
+	protected Element[] process(Element parentEl, String childName, Namespace ns, String cellKey, String separator) {
+		switch (cellKey) {
+		case "AvailTrans/FormatProfile":
+			return processFmtProfile(parentEl, childName, ns, cellKey, separator);
+		default:
+			return super.process(parentEl, childName, ns, cellKey, separator);
+
+		}
+
+	}
+
+	/**
+	 * @param parentEl
+	 * @param childName
+	 * @param ns
+	 * @param cellKey
+	 * @param separator
+	 * @return
+	 */
+	protected Element[] processFmtProfile(Element parentEl, String childName, Namespace ns, String cellKey,
+			String separator) {
+		// step 1: gen a single FormatProfile element the standard way
+		Element[] fmtProList = super.process(parentEl, childName, ns, cellKey, separator);
+		if (fmtProList == null || (fmtProList.length == 0)) {
+			return null;
+		}
+		Element fmtProEl = fmtProList[0];
+		// now add the optional arguments
+		addArgument(fmtProEl, "HDR", "AvailTrans/HDR");
+		addArgument(fmtProEl, "WCG", "AvailTrans/WCG");
+		addArgument(fmtProEl, "HFR", "AvailTrans/HFR");
+		addArgument(fmtProEl, "NGAudio", "AvailTrans/NGAudio");
+		return fmtProList;
+	}
+
+	/**
+	 * @param parentEl
+	 * @param attribute
+	 * @param cellKey
+	 */
+	protected void addArgument(Element parentEl, String attribute, String cellKey) {
+		Pedigree pg = getPedigreedData(cellKey);
+		if (pg == null) {
+			return;
+		}
+		String value = pg.getRawValue();
+		if (isSpecified(value)) {
+			parentEl.setAttribute(attribute, value);
+		}
+	}
 
 }
