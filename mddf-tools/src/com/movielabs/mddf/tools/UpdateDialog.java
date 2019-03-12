cc878d113845a00fa759d3e7a24f58978d9895bf
Larry Levin <lawrence.j.levin@gmail.com>
2019-03-12 10:41:04 -0400

    added checking for updates

---
+++ mddf-tools/src/com/movielabs/mddf/tools/UpdateDialog.java
@@ -0,0 +1,134 @@
+/**
+ * Copyright (c) 2019 MovieLabs
+
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.movielabs.mddf.tools;
+
+import javax.swing.JDialog;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+
+import javax.swing.JLabel;
+
+import net.sf.json.JSONObject;
+import net.sf.json.groovy.JsonSlurper;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+import java.awt.Insets;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class UpdateDialog extends JDialog {
+
+	/**
+	 * Launch the application. [for TESTING ONLY]
+	 */
+	public static void main(String[] args) {
+		try {
+			UpdateDialog dialog = new UpdateDialog();
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public UpdateDialog() {
+		String fakeStatus = "{\"latest\":{\"build.timestamp\":\"2019-Feb-27 19:16:36 UTC\",\"mddf.lib.version\":\"1.5.1.rc5-SNAPSHOT\",\"mddf.tool.build\":\"J\",\"mddf.tool.version\":\"1.5.1.rc4\"},\"jarUrl\":\"https://github.com/MovieLabs/mddf/raw/master/binaries/mddf-tools-1.5.1.rc4.jar\",\"status\":\"UPDATE\"}";
+		JsonSlurper slurper = new JsonSlurper();
+		JSONObject statusCheck = (JSONObject) slurper.parseText(fakeStatus);
+		initialize(statusCheck, "1.0.1", null);
+	}
+
+	public UpdateDialog(JSONObject statusCheck, String curVersion, Component parent) {
+		initialize(statusCheck, curVersion, parent);
+	}
+
+	private void initialize(JSONObject statusCheck, String curVersion, Component parent) {
+
+		setBounds(100, 100, 593, 300);
+		setIconImage(Toolkit.getDefaultToolkit()
+				.getImage(UpdateDialog.class.getResource("/com/movielabs/mddf/tools/images/icon_movielabs.jpg")));
+		setTitle("Update Available");
+		JSONObject latestReleaseDesc = statusCheck.getJSONObject("latest");
+		String jarUrl = statusCheck.getString("jarUrl");
+		String msg = "<html>A newer version of the MDDF Toolkit is available";
+		msg = msg + "<ul><li>Current version: " + curVersion + "</li>";
+		msg = msg + "<li>Latest release: " + latestReleaseDesc.getString("mddf.tool.version") + "</li></ul>";
+		msg = msg + "<p>To download the latest release, copy this URL, then paste it into you browser:<br/><a href='"
+				+ jarUrl + "'>" + jarUrl + "</a></p>";
+		msg = msg + "</html>";
+		JLabel lblAMoreRecent = new JLabel(msg);
+		lblAMoreRecent.setHorizontalAlignment(SwingConstants.CENTER);
+		getContentPane().add(lblAMoreRecent, BorderLayout.CENTER);
+
+		JPanel panel = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
+		flowLayout.setHgap(10);
+		getContentPane().add(panel, BorderLayout.SOUTH);
+
+		JButton copyBtn = new JButton("");
+		copyBtn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.out.println("Copying " + jarUrl);
+				StringSelection stringSelection = new StringSelection(jarUrl);
+				Clipboard clpbrd = Toolkit.getDefaultToolkit().getSystemClipboard();
+				clpbrd.setContents(stringSelection, null);
+			}
+		});
+		copyBtn.setMargin(new Insets(1, 0, 1, 0));
+		copyBtn.setToolTipText("Copy URL to clipboard");
+		copyBtn.setIcon(new ImageIcon(
+				UpdateDialog.class.getResource("/com/movielabs/mddf/tools/images/copy-to-clipboard.png")));
+		panel.add(copyBtn);
+
+		JButton btnOk = new JButton("OK");
+		btnOk.setMargin(new Insets(9, 12, 9, 12));
+		panel.add(btnOk);
+		btnOk.setSize(new Dimension(55, 25));
+		btnOk.setHorizontalAlignment(SwingConstants.RIGHT);
+
+		btnOk.setActionCommand("OK");
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(UpdateDialog.class.getResource("/com/movielabs/mddf/tools/images/attention-64.png")));
+		getContentPane().add(lblNewLabel, BorderLayout.NORTH);
+		btnOk.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+			}
+
+		});
+	}
+
+}
---
+++ mddf-tools/src/com/movielabs/mddf/tools/util/UpdateMgr.java
@@ -0,0 +1,167 @@
+/**
+ * Copyright (c) 2019 MovieLabs
+
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.movielabs.mddf.tools.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.Properties;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import com.movielabs.mddf.tools.ToolLauncher;
+import com.movielabs.mddf.tools.UpdateDialog;
+import com.movielabs.mddf.tools.ValidatorTool;
+
+import net.sf.json.JSON;
+import net.sf.json.JSONObject;
+import net.sf.json.groovy.JsonSlurper;
+
+/**
+ * @author L. Levin, Critical Architectures LLC
+ *
+ */
+public class UpdateMgr {
+
+	private static final String updateUrl = "https://mddf.movielabs.com/updateMgr"; 
+//	private static final String updateUrl = "http://localhost:8080/mddf-svcs/updateMgr";
+
+	private static final int maxDaysBtwnChecks = 7;
+
+	public static boolean check(ToolLauncher framework) {
+		Properties mddfToolProps = ValidatorTool.loadProperties("/com/movielabs/mddf/tools/build.properties");
+		// how long since last check??
+		String last = framework.getProperty("updateMgr.lastCheck");
+		if (last == null) {
+			// use the build time instead
+			last = mddfToolProps.getProperty("build.timestamp");
+		}
+		// just need yyyy-MMM-dd and can drop the hh:mm
+		String[] parts = last.split(" ");
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MMM-dd");
+		Date dateLastChecked = null;
+		try {
+			dateLastChecked = df.parse(parts[0]);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			return true;
+		}
+		LocalDate then = Instant.ofEpochMilli(dateLastChecked.getTime()).atZone(ZoneId.of("UTC+0")).toLocalDate();
+
+		// compare with current date
+		LocalDate now = LocalDate.now();
+		long daysBetween = ChronoUnit.DAYS.between(then, now);
+
+		System.out.println("ELAPSED DAYS=" + daysBetween);
+		if (daysBetween <= maxDaysBtwnChecks) {
+			// no need to check 
+			return true;
+		}
+		// check with server
+		String curVersion = mddfToolProps.getProperty("mddf.tool.version"); 
+		JSONObject statusCheck = getStatus(framework, "1.1.0");
+		if (statusCheck == null) {
+			/*
+			 * In the absence of information to the contrary, assume everything is
+			 * up-to-date
+			 */
+			return true;
+		}
+		String status = statusCheck.optString("status", "UPDATE");
+		if (status.equals("UPDATE")) {
+			// Notify user they need to update
+			UpdateDialog dialog = new UpdateDialog(statusCheck, curVersion, framework.getFrame());
+			dialog.setVisible(true);
+		}
+		System.out.println(statusCheck.toString(5));
+		return true;
+	} 
+
+	private static JSONObject getStatus(ToolLauncher framework, String curVersion) {
+		securityKludge();
+		String uuid = framework.getUUID();
+		String fullUrl = updateUrl + "?uuid=" + uuid + "&curVer=" + curVersion;
+		try {
+			URL netUrl = new URL(fullUrl);
+			InputStream is = netUrl.openStream();
+			JsonSlurper slurper = new JsonSlurper();
+			JSON response = slurper.parse(is);
+			if (response instanceof JSONObject) {
+				return ((JSONObject) response);
+			}
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/**
+	 * Handle problem of
+	 * 
+	 * <pre>
+	 * javax.net.ssl.SSLHandshakeException:
+	 *     sun.security.validator.ValidatorException: PKIX path building failed:
+	 *     sun.security.provider.certpath.SunCertPath
+	 * </pre>
+	 */
+	private static void securityKludge() {
+		// Create a trust manager that does not validate certificate chains
+		TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+			public X509Certificate[] getAcceptedIssuers() {
+				return null;
+			}
+
+			public void checkClientTrusted(X509Certificate[] certs, String authType) {
+			}
+
+			public void checkServerTrusted(X509Certificate[] certs, String authType) {
+			}
+		} };
+
+		// Install the all-trusting trust manager
+		try {
+			SSLContext sc = SSLContext.getInstance("TLS");
+			sc.init(null, trustAllCerts, new SecureRandom());
+			HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+		} catch (Exception e) {
+			;
+		}
+	}
+
+}
